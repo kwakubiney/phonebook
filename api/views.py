fd7b09dfc98399e27c043a62c8f5c9f33b817193
kb <71296367+kwakubiney@users.noreply.github.com>
2023-10-19 18:38:57 +0000

    feat: prevent non admins from performing certain operations (#3)

--- api/views.py
+++ api/views.py
@@ -3,17 +3,25 @@ from rest_framework import status, generics
 from api.models import PhoneBook as PhoneBookModel
 from api.serializers import PhoneBookSerializer
 from datetime import datetime
+from api.permissions import IsAdminOrReadOnly, IsDeleteAllowed
 
 class PhoneBook(generics.GenericAPIView):
     serializer_class = PhoneBookSerializer
     queryset = PhoneBookModel.objects.all()
     lookup_field = 'number'
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_phonebook_entry(self, number):
+        try:
+            return PhoneBookModel.objects.get(number=number)
+        except:
+            return None
 
     def get(self, request):
         entries = PhoneBookModel.objects.all()
-        serializer = self.serializer_class(entries)
+        serializer = self.serializer_class(entries, many=True)
         return Response({
-            "status_code": "1",
+            "status": "success",
             "data": serializer.data
         })
 
@@ -25,10 +33,22 @@ class PhoneBook(generics.GenericAPIView):
         else:
             return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
+    def patch(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            phonebook_entry = self.get_phonebook_entry(serializer.validated_data['number'])
+            if phonebook_entry == None:
+                return Response({"status_code": 0, "message": f"phone number specified does not exist"}, status=status.HTTP_404_NOT_FOUND)
+            serializer.validated_data['updated_at'] = datetime.now()
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
 class PhoneBookDetail(generics.GenericAPIView):
     queryset = PhoneBookModel.objects.all()
     serializer_class = PhoneBookSerializer
     lookup_field = 'number'
+    permission_classes = [IsDeleteAllowed]
 
     def get_phonebook_entry(self, number):
         try:
@@ -39,29 +59,16 @@ class PhoneBookDetail(generics.GenericAPIView):
     def get(self, request, number):
         phonebook_entry = self.get_phonebook_entry(number=number)
         if phonebook_entry == None:
-            return Response({"status_code": 0, "message": f"phone number requested for not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"status_code": 0, "message": f"phone number specified does not exist"}, status=status.HTTP_404_NOT_FOUND)
         serializer = self.serializer_class(phonebook_entry)
         if serializer.data["is_blacklisted"]:
             return Response({"status_code":1, "message": "blocked", "data":{"number": serializer.data["number"]}})
         else:
             return Response({"status_code":1, "message": "active", "data":{"number": serializer.data["number"]}})
 
-    def patch(self, request, number):
-        phonebook_entry = self.get_phonebook_entry(number)
-        if phonebook_entry == None:
-            return Response({"status_code": 0, "message": f"phone number requested for not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.serializer_class(
-            phonebook_entry, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.validated_data['updated_at'] = datetime.now()
-            serializer.save()
-            return Response({"status": "success", "data": serializer.data})
-        return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
     def delete(self, request, number):
-        phonebook_entry = self.get_phonebook_entry(number)
+        phonebook_entry = self.get_phonebook_entry(number=number)
         if phonebook_entry == None:
-            return Response({"status_code": 0, "message": f"phone number requested for not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"status_code": 0, "message": f"phone number specified does not exist"}, status=status.HTTP_404_NOT_FOUND)
         phonebook_entry.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
--- phonebook/settings.py
+++ phonebook/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
