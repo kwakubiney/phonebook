1a2c19532aed75f592da19aab3ba057437ee109c
kb <71296367+kwakubiney@users.noreply.github.com>
2023-10-19 11:45:47 +0000

    feat: create phonebook model (#1)

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class PhoneBook(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    number = models.CharField(max_length=100, default='')
+    is_blacklisted = models.BooleanField(default='white', max_length=100)
+    class Meta:
+        ordering = ['created_at']
